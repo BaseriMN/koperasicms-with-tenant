51f282eb52b6670c4d4b4bb21217cd98703d04dd
BaseriMN <muhamad.baseri@gmail.com>
2026-06-13 20:36:17 +0800

    fix: uncomment --force untuk seed tenant DB di production

--- config/tenancy.php
+++ config/tenancy.php
@@ -198,6 +198,6 @@ return [
      */
     'seeder_parameters' => [
         '--class' => 'DatabaseSeeder', // root seeder class
-        // '--force' => true, // This needs to be true to seed tenant databases in production
+        '--force' => true, // This needs to be true to seed tenant databases in production
     ],
 ];
